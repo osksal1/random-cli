d78319ba0e0c4e2edb6f5f7529d4bb6f37157641
osksal1 <oskarit17dt@gmail.com>
2019-08-09 12:31:38 +0300

    add ability to roll more random numbers

--- index.js
+++ index.js
@@ -10,10 +10,22 @@ inquirer
       type: "input",
       name: "numbers",
       message: "Number range:"
+    },
+    {
+        type: "input",
+        name: "rolls",
+        message: "How many numbers?"
     }
   ])
-  .then(({ numbers }) => {
+  .then(({ numbers,rolls }) => {
     const min = parseInt(numbers.split("-")[0]);
     const max = parseInt(numbers.split("-")[1]);
+    const rollAmount = parseInt(rolls);
+    if (rollAmount ===1)
     console.log(`Your random number is: ${getRandomInt(min, max)}`);
+    else {
+        for (let i = 0; i < rollAmount; i++) {
+            console.log(`Roll ${i}: ${getRandomInt(min, max)}`);
+        }
+    }
   });
